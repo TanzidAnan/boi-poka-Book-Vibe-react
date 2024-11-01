1b299571d3905d65f8a67d284c81ead72b457eb5
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-01 21:19:31 +0600

    book card

--- src/Compontents/Books/Books.jsx
+++ src/Compontents/Books/Books.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 const Books = () => {
     const [books,setBooks] =useState([]);
@@ -11,6 +12,11 @@ const Books = () => {
         <div>
             <h2 className="text-3xl font-semibold text-center">Books</h2>
             <p>{books.length}</p>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+                {
+                    books.map((book,idx) => <Book key={idx} book={book}></Book>)
+                }
+            </div>
         </div>
     );
 };
