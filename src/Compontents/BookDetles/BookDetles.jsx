96612c67c53589b259601e4e5ff245b68dba054d
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-02 18:51:17 +0600

    book detles

--- src/Compontents/BookDetles/BookDetles.jsx
+++ src/Compontents/BookDetles/BookDetles.jsx
@@ -1,8 +1,8 @@
 import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetles = () => {
-    const { bookId } = useParams();
-    const id =parseInt(bookId)
+    const { bookId, bookName, author, image, review, totalPages, rating, category, publisher, yearOfPublishing, tags } = useParams();
+    const id = parseInt(bookId)
     const data = useLoaderData();
 
     const book = data.find(book => book.bookId === id);
@@ -11,7 +11,45 @@ const BookDetles = () => {
 
     return (
         <div>
-            <h1>Book Details {bookId}</h1>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex justify-center">
+                    <div className="w-[50%] justify-center flex items-center">
+                        <img
+                            src={book.image}
+                            className="max-w-60 rounded-lg shadow-2xl" />
+                    </div>
+                    <div className="w-[50%] flex flex-col items-start mr-10">
+                        <h1 className="text-5xl font-bold mb-2">{book.bookName}</h1>
+                        <p className="py-6">by:{book.author}</p>
+                        <hr />
+                        <p>{book.category}</p>
+                        <hr />
+                        <p className="mt-4">{book.review}</p>
+                        <div className="flex gap-3">
+                            {
+                                book.tags.map(tag => <button className="flex btn btn-xs bg-green-300 text-green-900">#{tag}</button>)
+                            }
+                        </div>
+                        <div className="flex gap-9">
+                            <p>Number of Pages:</p>
+                            <p>{book.totalPages}</p>
+                        </div>
+                        <div className="flex gap-9">
+                            <p>Publisher:</p>
+                            <p>{book.publisher}</p>
+                        </div>
+                        <div className="flex gap-9">
+                            <p>Year of Publishing:</p>
+                            <p>{book.yearOfPublishing}</p>
+                        </div>
+                        <div>
+                            <button class="btn btn-active btn-accent">Read</button>
+                            <button className="btn btn-outline">Wishlist</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
