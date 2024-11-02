1ffc6dda3e19505434019901124e2cc8b15d99d8
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-02 18:03:05 +0600

    use prames

--- src/Compontents/BookDetles/BookDetles.jsx
+++ src/Compontents/BookDetles/BookDetles.jsx
@@ -1,8 +1,12 @@
+import { useParams } from "react-router-dom";
 
 const BookDetles = () => {
+const {bookId} =useParams();
+
+    console.log(bookId)
     return (
         <div>
-            
+            <h1>Book Details {bookId}</h1>
         </div>
     );
 };
