700d1fd0bce7744a2847560bdebe5492cf595c9f
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-26 23:21:15 +0600

    data load

--- src/Compontents/Dashboard/Dashboard.jsx
+++ src/Compontents/Dashboard/Dashboard.jsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
     const strogeData =getStrodeToWishList();
     const id =parseInt(strogeData);
     console.log(id)
-    const filterData =allData.filter(data => data.id == id)
+    const filterData =allData.filter(data => data.bookId == id)
     console.log(filterData)
     return (
         <div>
