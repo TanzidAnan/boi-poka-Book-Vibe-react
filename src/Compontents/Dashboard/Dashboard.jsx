46fff42cd3e40e9a0bdc8ae1ca737eb0a356d92c
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-26 23:10:36 +0600

    data load

--- src/Compontents/Dashboard/Dashboard.jsx
+++ src/Compontents/Dashboard/Dashboard.jsx
@@ -1,9 +1,12 @@
 import { Helmet } from "react-helmet-async";
 import { useLoaderData } from "react-router-dom";
+import { getStroedReadList } from "../../Utility/AddToDb";
 
 const Dashboard = () => {
     const allData =useLoaderData();
     console.log(allData)
+    const strogeData =getStroedReadList();
+    console.log(strogeData)
     return (
         <div>
             <Helmet>
